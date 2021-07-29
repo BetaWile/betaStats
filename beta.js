c67c584629fe89cd010ba8e72e816c8fda20f049
Beta <77292192+BetaWile@users.noreply.github.com>
2021-07-29 05:30:17 +0300

    Update beta.js

--- beta.js
+++ beta.js
@@ -44,7 +44,7 @@ fs.readdir('./src/Command', (err, files) => {
   });
 
 client.on('message', async(message) => {
-  if(!message.guild || message.author.bot || message.content.startsWith(Main.prefix)) return;
+  if(!message.guild || message.author.bot || message.content.startsWith(Main.Prefix)) return;
   db.add(`messageData.${message.author.id}.channel.${message.channel.id}`, 1);
   db.push(`messageData.${message.author.id}.times`, {time: Date.now(), puan: 1})
 });
